810527d0b02c37c7e1718f22e3c3d0331ae30f6d
Tom1363 <tomsuzuki2633@gmail.com>
2018-05-20 02:57:44 +0900

    消した

--- tester.py
+++
@@ -1,9 +0,0 @@
-import os
-
-filepath = "E:/AI/venv/chatbot_test/learn_txts"
-filename = os.listdir(filepath)
-num = 0
-for names in filename:
-    with open(filepath + "/" + names, 'r', encoding="utf-8_sig") as f:
-        datas = f.read()
-        print(datas)
